6d36d17a062d1ac0ff1f767fcc9437ea2c7607c8
Tung Phan <tung@caltech.edu>
2019-04-14 15:39:52 -0700

    cleanup

--- contracts/mutate.py
+++ contracts/mutate.py
@@ -1,6 +1,7 @@
 # Tung Phan
 # April 11, 2019
 # This module generates jumps in assertions
+
 import os
 current_path = os.path.dirname(os.path.abspath(__file__))
 
@@ -17,61 +18,53 @@ assumptions['button1']  = r"""
 # button1 behavior
 ((x1 = 0 & y1 = 1) | (x2 = 0 & y2 = 1)) -> bridge'
 """
-
 assumptions['button2']  = r"""
 [ENV_TRANS]
 # button2 behavior
 ((x1=0 & y1=3) | (x2=0 & y2=3)) -> bridge'
 """
-
 assumptions['init'] = r"""
 [ENV_INIT]
 !bridge
 box=0
 """
-
 assumptions['ego1_start'] = r"""
 [ENV_INIT]
 x1<=2
 y1<=1
 """
-
 assumptions['ego1_near'] = r"""
 [ENV_INIT]
 x1<=1
 y1>=3
 """
-
 assumptions['ego1_far'] = r"""
 [ENV_INIT]
 x1>=3
 y1>=3
 """
-
 assumptions['ego2_start'] = r"""
 [ENV_INIT]
 x2<=2
 y2<=1
 """
-
 assumptions['ego2_near'] = r"""
 [ENV_INIT]
 x2<=1
 y2>=3
 """
-
 assumptions['ego2_far'] = r"""
 [ENV_INIT]
 x2>=3
 y2>=3
 """
-
 assumptions['bridge_broken'] = r"""
 [SYS_TRANS]
 # far bridge is
 !(x1=2 & y1=4)
 !(x2=2 & y2=4)
 """
+
 guarantees['box_far'] = r"""
 [SYS_LIVENESS]
 box=3
@@ -96,7 +89,6 @@ guarantees['box_ego2_near'] = r"""
 [SYS_LIVENESS]
 x2=0 & y2=4 & box=2
 """
-
 guarantees['box_ego2_far'] = r"""
 [SYS_LIVENESS]
 x2 = 4 & y2 =3 & box=3

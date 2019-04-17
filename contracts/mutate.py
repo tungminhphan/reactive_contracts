df9cd3f6d12140806f71ea757b8129aaeab7c084
Tung Phan <tung@caltech.edu>
2019-04-16 21:30:44 -0700

    add AG pairs

--- contracts/mutate.py
+++ contracts/mutate.py
@@ -5,29 +5,12 @@
 import os
 current_path = os.path.dirname(os.path.abspath(__file__))
 
-# load template
-inputfile = open(current_path + '/AG_template.structuredslugs')
 # write to new contract
 outputfile = open(current_path + '/AG_contract.structuredslugs', 'w')
 
 assumptions = dict()
 guarantees = dict()
 
-assumptions['button1']  = r"""
-[ENV_TRANS]
-# button1 behavior
-((x1 = 0 & y1 = 1) | (x2 = 0 & y2 = 1)) -> bridge'
-"""
-assumptions['button2']  = r"""
-[ENV_TRANS]
-# button2 behavior
-((x1=0 & y1=3) | (x2=0 & y2=3)) -> bridge'
-"""
-assumptions['init'] = r"""
-[ENV_INIT]
-!bridge
-box=0
-"""
 assumptions['ego1_start'] = r"""
 [ENV_INIT]
 x1<=2
@@ -58,6 +41,16 @@ assumptions['ego2_far'] = r"""
 x2>=3
 y2>=3
 """
+assumptions['button1_working']  = r"""
+[ENV_TRANS]
+# button1 behavior
+((x1 = 0 & y1 = 1) | (x2 = 0 & y2 = 1)) -> bridge'
+"""
+assumptions['button2_working']  = r"""
+[ENV_TRANS]
+# button2 behavior
+((x1=0 & y1=3) | (x2=0 & y2=3)) -> bridge'
+"""
 assumptions['bridge_broken'] = r"""
 [SYS_TRANS]
 # far bridge is
@@ -81,7 +74,7 @@ guarantees['ego2_near'] = r"""
 [SYS_LIVENESS]
 x2=0 & y2=4
 """
-guarantees['ego2_recharge'] = r"""
+guarantees['ego2_start'] = r"""
 [SYS_LIVENESS]
 x2=2 & y2=0
 """
@@ -94,24 +87,79 @@ guarantees['box_ego2_far'] = r"""
 x2 = 4 & y2 =3 & box=3
 """
 
-outputfile.writelines(inputfile)
+def get_assumptions():
+    loc = {'start', 'near', 'far'}
+    safe = {'button1', 'button2', 'bridge'}
+    assumptions = []
+    for ego1 in loc:
+        ego1 = 'ego1_' + ego1
+        for ego2 in loc:
+            ego2 = 'ego2_' + ego2
+            for button1_working in {True, False}:
+                for button2_working in {True, False}:
+                    for bridge_working in {True, False}:
+                        assumption = [ego1, ego2]
+                        if button1_working:
+                            assumption.append('button1_working')
+                        if button2_working:
+                            assumption.append('button2_working')
+                        if bridge_working:
+                            assumption.append('bridge_working')
+                        assumptions.append(assumption)
+    return assumptions
 
-# ASSUMPTIONS
-outputfile.writelines(assumptions['init'])
-outputfile.writelines(assumptions['ego1_near'])
-outputfile.writelines(assumptions['ego2_far'])
-outputfile.writelines(assumptions['button1'])
-outputfile.writelines(assumptions['button2'])
-#outputfile.writelines(assumptions['bridge_broken'])
+def get_guarantees():
+    loc_box = {'near', 'far', None}
+    loc2 = {'start', 'near', 'far', None}
+    guarantees = []
+    for ego2 in loc2:
+        if ego2 != None:
+           ego2 = 'ego2_' + ego2
+        for box in loc_box:
+            if box != None:
+                box = 'box_' + box
+            for box_ego2 in loc_box:
+                if box_ego2 != None:
+                    choice = box_ego2
+                    box_ego2 = 'box_ego2_' + box_ego2
+                    if box == 'box_' + choice:
+                        box = None
+                    if ego2 == 'ego2_' + choice:
+                        ego2 = None
+                guarantee = []
+                if box_ego2 != None:
+                    guarantee.append(box_ego2)
+                if ego2 != None:
+                    guarantee.append(ego2)
+                if box != None:
+                    guarantee.append(box)
+                if guarantee != []:
+                    guarantees.append(guarantee)
+    return guarantees
 
-# GUARANTEES
-outputfile.writelines(guarantees['box_near'])
-#outputfile.writelines(guarantees['box_far'])
-outputfile.writelines(guarantees['ego2_recharge'])
-#outputfile.writelines(guarantees['ego2_far'])
-outputfile.writelines(guarantees['ego2_near'])
-outputfile.writelines(guarantees['box_ego2_near'])
-#outputfile.writelines(guarantees['box_ego2_far'])
+Ai = get_assumptions()
+Gi = get_guarantees()
+
+
+def synthesize_contract(A,G):
+    # load template
+    inputfile = open(current_path + '/AG_template.structuredslugs')
+    outputfile.writelines(inputfile)
+    if 'bridge_working' not in set(A): # negative properties
+        outputfile.writelines(assumptions['bridge_broken'])
+    for a in A:
+        if a == "bridge_working":
+            pass
+        else:
+            outputfile.writelines(assumptions[a])
+    for g in G:
+        outputfile.writelines(guarantees[g])
+
+A = Ai[50]
+G = Gi[3]
+print(A)
+print(G)
+synthesize_contract(A,G)
 
 inputfile.close()
 outputfile.close()
